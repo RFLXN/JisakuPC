9c948ebe216fbc14ed64c0e324287f7a150dd135
jc19304024 <jc19304024@ga.tera-house.ac.jp>
2020-11-17 15:12:05 +0900

    sql connection test

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,5 @@
 import db.MySQLConnector;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
