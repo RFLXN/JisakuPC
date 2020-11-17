09687d2782e264c1905f5d7e730b3f0fb2d51162
jc19304024 <jc19304024@ga.tera-house.ac.jp>
2020-11-17 11:26:37 +0900

    init

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,12 @@
-public class Test {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+public class Test extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getOutputStream().println("<h1>Hello World!</h1>");
+    }
 }
