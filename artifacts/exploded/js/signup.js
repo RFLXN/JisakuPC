4e9abaca072b2095971523f0e7e29596b3107cc5
jc19304024 <jc19304024@ga.tera-house.ac.jp>
2021-01-15 11:15:58 +0900

    signup commands

---
+++ artifacts/exploded/js/signup.js
@@ -0,0 +1,32 @@
+function signup() {
+    const userId = $("#signup-id").val();
+    console.log(userId);
+
+    // ユーザーIDがもう使われているかをチェックする ajax処理
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "json/signup-check",
+        data: {
+            id: userId
+        },
+        success: function (data) {
+            const result = data.isAlreadyUsedId;
+
+            console.log(result);
+
+            if(result) {        // もう使われている場合 エラーメッセージを出力
+                $("#warning-text").text("もう使われているIDです。");
+            } else {            // 問題ない場合 転送する
+                submitForm();
+            }
+        },
+        error: function (data) {
+            console.log("Some Error Occurred");
+        }
+    });
+}
+
+function submitForm() {
+    $("#signup-form").submit();
+}
