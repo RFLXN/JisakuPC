3e53ed06e920e8e748636897878ea8c099bbffde
jc19304024 <jc19304024@ga.tera-house.ac.jp>
2021-02-12 11:13:03 +0900

    commit

---
+++ sql/data/orcl_build_table.sql
@@ -0,0 +1,3 @@
+insert into orcl.build_table (build_no, user_no, build_name)
+values  (1, 1, 'build1'),
+        (2, 2, 'build2');
---
+++ sql/data/orcl_user_table.sql
@@ -0,0 +1,7 @@
+insert into orcl.user_table (user_no, user_id, user_pw, admin)
+values  (1, 'userid1', '12654c024a4c0926329753cf79d4bb95d617c17684066809cf453b2084a4d5bc', 0),
+        (2, 'userid2', 'd6e5afe89ed25da45a4f3783ac4d9d961511eca9009d59efc257ed2af94ce901', 0),
+        (3, 'admin', '713bfda78870bf9d1b261f565286f85e97ee614efe5f0faf7c34e7ca4f65baca', 1),
+        (6, 'asd', '688787d8ff144c502c7f5cffaafe2cc588d86079f9de88304c26b0cb99ce91c6', 0),
+        (8, 'userid13', '688787d8ff144c502c7f5cffaafe2cc588d86079f9de88304c26b0cb99ce91c6', 0),
+        (9, 'userid11', 'ded2fa853916f691438772eaa2c1c1f2922c44f4e662a80a24d3a94ed1e31a5c', 0);
