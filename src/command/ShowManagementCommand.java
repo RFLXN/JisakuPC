a42ffbe189cad7b8eb0164299fbcdd5cec18857e
hs19304020 <hs19304020@ga.tera-house.ac.jp>
2021-03-03 15:08:36 +0900

    非ログイン状態で管理者ページのURLをうつと飛べていたため対応

--- src/command/ShowManagementCommand.java
+++ src/command/ShowManagementCommand.java
@@ -1,12 +1,20 @@
 package command;
 
+import javax.servlet.http.HttpServletRequest;
+
+import bean.UserFlag;
 import context.ResponseContext;
 
 public class ShowManagementCommand extends AbstractCommand {
     @Override
     public ResponseContext execute(ResponseContext responseContext) throws CommandException {
-
-        responseContext.setTarget("admin/productmanagement");
+        boolean userNo = ((UserFlag) ((HttpServletRequest) getRequestContext().getRequest())
+                .getSession().getAttribute("loginFlag")).isAdmin();
+        if(userNo == true) {
+            responseContext.setTarget("admin/productmanagement");
+        } else {
+        	responseContext.setTarget("/JisakuPC/");
+        }
         return responseContext;
     }
 }
